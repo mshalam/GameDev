0448b206e29722a49f309d82587f2ca9d63c9438
Maurice Shalam <mss619@nyu.edu>
2014-07-14 15:01:33 -0400

    homework one added

---
+++ week01a/homework 1/Assets/scripts/poemManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class poemManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		Debug.Log("Hello World");
+
+		GetComponent<TextMesh>().text = "The sight of the hills is a beauty \nThe bright moon is the sign of purity \nThe flow of the river is a murmuring music \nThe act of the sun is so philanthropic.";
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
